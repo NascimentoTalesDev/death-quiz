add70e684ab2e5cd09f70e9ef8f1ea87aaa58906
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-07-25 15:06:36 -0400

    feat: add save user data

--- src/app/services/auth/index.ts
+++ src/app/services/auth/index.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "@/lib/prismadb"
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [
