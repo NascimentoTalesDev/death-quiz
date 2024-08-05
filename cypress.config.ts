9357d11f86bc6d3ba37cda403757189b0650823e
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-08-05 15:49:00 -0400

    comment cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,12 @@
-import { defineConfig } from "cypress";
+// import { defineConfig } from "cypress";
 
-export default defineConfig({
-  chromeWebSecurity: false,
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
-    baseUrl: "https://death-quiz.vercel.app/auth",
-    testIsolation: false
-  },
-});
+// export default defineConfig({
+//   chromeWebSecurity: false,
+//   e2e: {
+//     setupNodeEvents(on, config) {
+//       // implement node event listeners here
+//     },
+//     baseUrl: "https://death-quiz.vercel.app/auth",
+//     testIsolation: false
+//   },
+// });
