af104030b881e60595a7a26c7fb45fb766601ed6
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-08-16 13:12:06 -0400

    feat: remove ProfileUser

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,4 @@
 import LatestQuizAdded from "@/components/dashboard/home/LatestQuizAdded";
-import Profile from "@/components/dashboard/home/Profile";
-import { useCurrentUser } from "@/hooks/use-current-user";
-import { findUserById } from "./actions";
 
 export default async function DashboardPage() {
   
