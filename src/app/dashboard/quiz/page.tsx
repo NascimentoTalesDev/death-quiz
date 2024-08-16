bdc34903150cdc47b97615db916bb13d78391eb4
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-08-16 14:15:52 -0400

    feat: remove favoritepage

--- src/app/dashboard/quiz/page.tsx
+++ src/app/dashboard/quiz/page.tsx
@@ -1,14 +1,10 @@
-import QuizGroup from '@/components/dashboard/quiz/QuizGroup'
 import React from 'react'
-import getAllQuizzes from './actions'
 
 const QuizPage = async() => {
-  const quizzes = await getAllQuizzes()
   
   return (
     <div className=''>
       <h1 className='mb-5'>Todos os Quizzes</h1>
-      <QuizGroup quizzes={quizzes}/>
     </div>
   )
 }
