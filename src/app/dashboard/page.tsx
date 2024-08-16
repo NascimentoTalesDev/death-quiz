2d8b860d710b6a333a70437539a25f38eb80499f
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-08-16 13:51:14 -0400

    feat: remove favoritepage

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,7 @@
-import LatestQuizAdded from "@/components/dashboard/home/LatestQuizAdded";
-
 export default async function DashboardPage() {  
   return (
     <div className="h-full w-full">
-        {/* <Profile user={user} /> */}
-        <LatestQuizAdded />
+        
     </div>
   );
 }
