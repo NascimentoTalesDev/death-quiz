37788b2d544618b62ee5b8087f0740702b8d71f2
NascimentoTalesDev <111396464+NascimentoTalesDev@users.noreply.github.com>
2024-08-16 13:14:49 -0400

    feat: remove latest

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,8 @@
-import LatestQuizAdded from "@/components/dashboard/home/LatestQuizAdded";
-
 export default async function DashboardPage() {
   
   return (
     <div className="h-full w-full">
-        <LatestQuizAdded />
+        
     </div>
   );
 }
