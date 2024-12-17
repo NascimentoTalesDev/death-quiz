8f49dd33078489bfe55d35d546b767a6f86e3f8d
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-12-17 00:25:34 -0300

    feat: add database

--- src/utils/base-url.ts
+++ src/utils/base-url.ts
@@ -1,1 +1,1 @@
-export const baseUrl = process.env.NODE_ENV === "production" ? "https://death-quiz-api.vercel.app" : "http://localhost:8000"
+export const baseUrl = process.env.NODE_ENV === "production" ? "https://api-death-quiz.vercel.app" : "https://api-death-quiz.vercel.app"
