85541dcbe754c24d07be90a74439248ecff80ad5
NascimentoTalesDev <nascimentotalesdev@gmail.com>
2024-12-19 17:05:11 -0300

    feat: add actions adminQuiz

--- src/app/admin/dashboard/quizzes/[id]/questions/actions.ts
+++ src/app/admin/dashboard/quizzes/[id]/questions/actions.ts
@@ -4,18 +4,46 @@ import { baseUrl } from "@/utils/base-url";
 import { revalidatePath } from "next/cache";
 
 export async function updateQuestion(values: { correctAnswer: string; questionId: number; quizId: number | null; question: string; answers: { text: string; }[] }){  
+  const { questionId, correctAnswer, question, answers  } =  values;
   
-  const res = await fetch(`${baseUrl}/questions/${values.questionId}`, {
-      method: 'PATCH',
-      headers: {
-          'Content-Type': 'application/json',
-          'API-Key': process.env.DATA_API_KEY!,
-      },
-      body: JSON.stringify(values)
+  // const res = await fetch(`${baseUrl}/questions/${values.questionId}`, {
+  //     method: 'PATCH',
+  //     headers: {
+  //         'Content-Type': 'application/json',
+  //         'API-Key': process.env.DATA_API_KEY!,
+  //     },
+  //     body: JSON.stringify(values)
+  // })
+  // const { correctAnswer, question, answers  } =  updateQuestionDto;
+
+  const updatedQuestion = await prismadb.question.update({
+    where:{
+      id: questionId
+    },
+    data:{
+      correctAnswer,
+      question
+    }
+  });
+
+  await prismadb.answer.deleteMany({      
+    where:{
+      questionId: values.questionId
+    }
   })
+
+  await Promise.all(answers.map(answer => 
+    prismadb.answer.create({      
+      data:{
+        questionId,
+        answer: answer.text
+      }
+    })
+  ));
+
   revalidatePath(`/admin/dashboard/quizzes/${values.quizId}/questions/${values.questionId}`)
-  const questionUpdated = await res.json()    
-  return questionUpdated
+  // const updatedQuestion = await res.json()    
+  return updatedQuestion
 }
 
 export async function deleteQuestion(quizId: number, questionId: number){  
--- src/app/admin/dashboard/quizzes/new-quiz/actions.ts
+++ src/app/admin/dashboard/quizzes/new-quiz/actions.ts
@@ -4,72 +4,82 @@ import { baseUrl } from "@/utils/base-url";
 import { Quiz } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
-export async function createQuiz(values: Quiz){
+export async function createQuiz(values: Quiz) {
   const res = await fetch(`${baseUrl}/quizzes`, {
-      method: 'POST',
-      headers: {
-          'Content-Type': 'application/json',
-          'API-Key': process.env.DATA_API_KEY!,
-      },
-      body: JSON.stringify(values)
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': process.env.DATA_API_KEY!,
+    },
+    body: JSON.stringify(values)
   })
   revalidatePath('/admin/dashboard/quizzes')
   revalidatePath('/dashboard/quizzes')
-  const quizzes = await res.json()    
+  const quizzes = await res.json()
   return quizzes
 }
 
-export async function updateQuiz(id: number, values: Quiz){
-  const res = await fetch(`${baseUrl}/quizzes/${id}`, {
-      method: 'PATCH',
-      headers: {
-          'Content-Type': 'application/json',
-          'API-Key': process.env.DATA_API_KEY!,
-      },
-      body: JSON.stringify(values)
+export async function updateQuiz(id: number, values: Quiz) {
+  // const res = await fetch(`${baseUrl}/quizzes/${id}`, {
+  //     method: 'PATCH',
+  //     headers: {
+  //         'Content-Type': 'application/json',
+  //         'API-Key': process.env.DATA_API_KEY!,
+  //     },
+  //     body: JSON.stringify(values)
+  // })
+
+  const quizUpdated = await prismadb.quiz.update({
+    where: {
+      id
+    },
+    data: {
+      title: values.title,
+      image: values.image
+    },
   })
+
   revalidatePath('/admin/dashboard/quizzes')
   revalidatePath('/dashboard/quizzes')
-  const quizUpdated = await res.json()    
+  // const quizUpdated = await res.json()
   return quizUpdated
 }
 
-export default async function getAllQuizzesAmin(){
+export default async function getAllQuizzesAmin() {
   const res = await fetch(`${baseUrl}/quizzes/admin`, {
-      method: 'GET',
-      headers: {
-          'Content-Type': 'application/json',
-          'API-Key': process.env.DATA_API_KEY!,
-          'Cache-Control': 'no-cache',
-      },
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'API-Key': process.env.DATA_API_KEY!,
+      'Cache-Control': 'no-cache',
+    },
   })
-  const quizzes = await res.json()    
+  const quizzes = await res.json()
   return quizzes
 }
 
-export async function upload(formData: FormData){  
+export async function upload(formData: FormData) {
   const response = await fetch(`${baseUrl}/uploads`, {
-      method: 'POST',
-      headers: {
-        'API-Key': process.env.DATA_API_KEY!,
-      },
-      body: formData
+    method: 'POST',
+    headers: {
+      'API-Key': process.env.DATA_API_KEY!,
+    },
+    body: formData
   });
-  const res = response.json()  
+  const res = response.json()
   return res;
 }
 
-export async function deleteImage(image: string){
+export async function deleteImage(image: string) {
   const id = image.split('.com/')[1];
-  
+
   const response = await fetch(`${baseUrl}/uploads/${id}`, {
-      method: 'DELETE',
-      headers: {
-        'API-Key': process.env.DATA_API_KEY!,
-      },
+    method: 'DELETE',
+    headers: {
+      'API-Key': process.env.DATA_API_KEY!,
+    },
   });
   const res = response.json()
-  
   return res;
 }
 
